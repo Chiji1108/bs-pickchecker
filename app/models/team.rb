2d569dcc258e705f39ca7df6c42fd88de00b083a
Chiji1108 <shingo.pad@gmail.com>
2020-01-05 01:42:05 +0900

    update

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,5 +5,5 @@ class Team < ApplicationRecord
   has_many :accounts, through: :picks
   has_many :brawlers, through: :picks
 
-  validates :picks, uniqueness: true
+  # validates :picks, uniqueness: true
 end
--- app/models/team_battle_history.rb
+++ app/models/team_battle_history.rb
@@ -3,7 +3,7 @@ class TeamBattleHistory < ApplicationRecord
   belongs_to :battle
 
   validates :team, presence: true
-  validates :battle, presences: true
+  validates :battle, presence: true
   
   validates :id, uniqueness: {scope: [:team, :battle, :result]}
 end
